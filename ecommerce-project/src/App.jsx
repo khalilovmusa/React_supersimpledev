fcb23a0b121a3e6da5d0aa5f8f3de42c4eafd68b
Musa <Musaxlilov29@gmail.com>
2025-08-21 16:53:02 +0400

    Checkout add and css

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import HomePage from './pages/HomePage'
+import CheckoutPage from './pages/CheckoutPage'
 
 function App() {
 
   return (
     <Routes>
       <Route element={<HomePage />} index />
-      <Route element={<div>Test checkout</div>} path="checkout" />
+      <Route element={<CheckoutPage />} path="checkout" />
     </Routes>
   )
 }
